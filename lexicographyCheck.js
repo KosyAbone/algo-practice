674075533ce16b2008877561cc19c989c4037182
KosyAbone <KosyAbone>
2022-03-01 13:23:56 +0100

    added ques

--- lexicographyCheck.js
+++ lexicographyCheck.js
@@ -1,4 +1,8 @@
-/*Given two strings s and t, both consisting of lowercase English letters and digits, your task is to calculate how many ways exactly one digit could be removed from one of the strings so that s is lexicographically smaller than t after the removal. Note that we are removing only a single instance of a single digit, rather than all instances (eg: removing 1 from the string a11b1c could result in a1b1c or a11bc, but not abc).
+/*Given two strings s and t, both consisting of lowercase English letters and digits, your task is to 
+calculate how many ways exactly one digit could be removed from one of the strings so that s is 
+lexicographically smaller than t after the removal. Note that we are removing only a single instance 
+of a single digit, rather than all instances (eg: removing 1 from the string a11b1c could result in 
+a1b1c or a11bc, but not abc).
 
 Also note that digits are considered lexicographically smaller than letters.
 
@@ -15,4 +19,10 @@ We can remove the second digit from t, obtaining "1zz56". "ab12c" > "1zz56", so 
 We can remove the third digit from t, obtaining "1zz46". "ab12c" > "1zz46", so we don't count this removal
 We can remove the fourth digit from t, obtaining "1zz45". "ab12c" > "1zz45", so we don't count this removal
 The only valid case where s < t after removing a digit is "ab12c" < "zz456". Therefore, the answer is 1.*/
-
+function solution(s,t){
+    for(let i = 0; i < s.length || i < t.length; i++)
+        
+        console.log(s.charCodeAt(i))
+        console.log(t.charCodeAt(i))
+}
+console.log(solution("ab12c", "1zz456"))
